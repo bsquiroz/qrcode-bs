de3953b2cfaa15a9b27163846781fa02c544b897
bsquiroz <bemq1605@gmail.com>
2024-04-18 21:42:36 -0500

    add header

---
+++ src/qrcodeapp/components/Header/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="header">
+    <a class="logo" href="https://github.com/bsquiroz" target="_blank"
+      >qrcode-bs</a
+    >
+    <a
+      class="icon__github"
+      href="https://github.com/bsquiroz/qrcode-bs"
+      target="_blank"
+    >
+      <i class="bx bxl-github"></i>
+    </a>
+  </header>
+</template>
+
+<style scope>
+.header {
+  @apply flex justify-between items-center;
+}
+
+.logo {
+  @apply font-extrabold uppercase bg-gradient-to-r from-yellow-500 to-white text-transparent bg-clip-text;
+}
+
+.icon__github {
+  @apply text-2xl;
+}
+</style>
--- src/qrcodeapp/components/Layout/Layout.vue
+++ src/qrcodeapp/components/Layout/Layout.vue
@@ -1,6 +1,11 @@
+<script setup lang="ts">
+import Header from "../Header/Header.vue";
+</script>
+
 <template>
   <main class="bg-gray-900 text-white">
     <section class="max-w-7xl min-h-screen m-auto p-5">
+      <Header />
       <slot />
     </section>
   </main>
